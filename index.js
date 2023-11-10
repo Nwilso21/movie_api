29fa69bb2e2afdf894ed53163288d0e5e4389e1b
Nwilso21 <noahwilson6381@gmail.com>
2023-11-09 18:44:29 -0700

    Updated URI
    Changed the uri to be a secure variable

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const Users = Models.User;
 
 const {check, validationResult} = require('express-validator');
 
-mongoose.connect("mongodb://127.0.0.1/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require("express"),
   morgan = require("morgan"),

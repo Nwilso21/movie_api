1b528009a67b2d532b44a7bcf369746b66ee7521
Nwilso21 <noahwilson6381@gmail.com>
2023-12-17 19:46:44 -0700

    Cors change
    Testing a new cors configuration to verify everything is working as it should. Will not be final

--- index.js
+++ index.js
@@ -26,20 +26,8 @@ app.use(
   })
 );
 
-const cors = require("cors");
-let allowedOrigins = ['http://localhost:1234', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
-
+const cors = require('cors');
+app.use(cors());
 
 let auth = require("./auth")(app);
 const passport = require("passport");
